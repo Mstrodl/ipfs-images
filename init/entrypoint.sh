098731420da01a6eb50da96368a3619cf999ab76
Mary Strodl <ipadlover8322@gmail.com>
2024-01-31 13:17:44 -0500

    add auth proxy

--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-PORT=31354
-jq --arg ip "$(curl ipinfo.io | jq -r .ip)" --arg tcpPort $PORT --arg udpPort $PORT '.Addresses.AppendAnnounce |= [
-  "/ip4/" + $ip + "/tcp/" + $tcpPort,
-  "/ip4/" + $ip + "/udp/" + $udpPort + "/quic",
-  "/ip4/" + $ip + "/udp/" + $udpPort + "/quic-v1",
-  "/ip4/" + $ip + "/udp/" + $udpPort + "/quic-v1/webtransport"
-]' /data/ipfs/config | sponge /data/ipfs/config
+if [ -f /data/ipfs/config ]; then
+  PORT=31354
+  jq --arg ip "$(curl ipinfo.io | jq -r .ip)" --arg tcpPort $PORT --arg udpPort $PORT '.Addresses.AppendAnnounce |= [
+    "/ip4/" + $ip + "/tcp/" + $tcpPort,
+    "/ip4/" + $ip + "/udp/" + $udpPort + "/quic",
+    "/ip4/" + $ip + "/udp/" + $udpPort + "/quic-v1",
+    "/ip4/" + $ip + "/udp/" + $udpPort + "/quic-v1/webtransport"
+  ]' /data/ipfs/config | sponge /data/ipfs/config
+fi
