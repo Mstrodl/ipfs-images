faf9d59a811c341c64c5bcb52ea8dcc92fb121ed
Mary Strodl <ipadlover8322@gmail.com>
2024-01-30 20:13:33 -0500

    add curl

--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-jq --arg IP "$(curl ipinfo.io | jq -r .ip)" '.Addresses.AppendAnnounce |= [
+PORT=31354
+jq --arg ip --arg tcpPort $PORT --arg udpPort $PORT "$(curl ipinfo.io | jq -r .ip)" '.Addresses.AppendAnnounce |= [
   "/ip4/" + $ip + "/tcp/" + $tcpPort,
   "/ip4/" + $ip + "/udp/" + $udpPort + "/quic",
   "/ip4/" + $ip + "/udp/" + $udpPort + "/quic-v1",
